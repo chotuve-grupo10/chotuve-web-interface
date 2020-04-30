d9c35d6ac4960c40c62a579983549784802b88a0
leandro <ltessore@github.com>
2020-04-30 10:17:42 -0300

    Se modifica el index, se agrega button de ping(sin direccionamiento)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Card from './components/Card'
 
-const product = {
-  name: 'Chotuve',
-  imagen: 'https://as01.epimg.net/meristation/imagenes/2019/11/12/betech/1573554822_627666_1573555077_noticia_normal.jpg' ,
-} 
-
-function getName(user){
-  return `${product.name}`
-}
-
-
-const element = <h1>Bienvenidos a {getName(product)} </h1>
 const container = document.getElementById('root')
 
-ReactDOM.render(element,container)
+ReactDOM.render(<Card />,container)
