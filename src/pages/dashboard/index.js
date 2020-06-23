bd78f7ae57a78fef2c256a39f037066a6a51ceb2
Diego Alfonso <diego.alfonso2@gmail.com>
2020-06-23 02:15:48 -0300

    Comento temporalmente el Redirect en src/pages/dashboard/index.js para que pase el build

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Link, Redirect} from 'react-router-dom'
+// import {Link, Redirect} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 class Dashboard extends React.Component{
 
