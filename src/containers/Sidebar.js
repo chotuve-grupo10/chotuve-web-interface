fd9e532d75f308db62ef42caa80fea8f77bf84aa
Diego Alfonso <diego.alfonso2@gmail.com>
2020-06-28 23:10:32 -0300

    Version 0 del layout

---
+++ src/containers/Sidebar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import 'font-awesome/css/font-awesome.min.css';
+import Home from '../views/Home'
+import AppServers from '../views/AppServers'
+
+class Sidebar extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Route render={({ location, history }) => (
+            <React.Fragment>
+                <SideNav
+                    onSelect={(selected) => {
+                        const to = '/' + selected;
+                        if (location.pathname !== to) {
+                            history.push(to);
+                        }
+                    }}
+                >
+                    <SideNav.Toggle />
+                    <SideNav.Nav defaultSelected="home">
+                        <NavItem eventKey="home">
+                            <NavIcon>
+                                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                            </NavIcon>
+                            <NavText>
+                                Home
+                            </NavText>
+                        </NavItem>
+                        <NavItem eventKey="app-servers">
+                            <NavIcon>
+                                <i className="fa fa-fw fa-server" style={{ fontSize: '1.75em' }} />
+                            </NavIcon>
+                            <NavText>
+                                Devices
+                            </NavText>
+                        </NavItem>
+                    </SideNav.Nav>
+                </SideNav>
+                <main>
+                    <Route path="/home" component={props => <Home />} />
+                    <Route path="/app-servers" component={props => <AppServers />} />
+                </main>
+            </React.Fragment>
+        )}
+        />
+      </Router>
+    )
+  }
+}
+
+export default Sidebar;
--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 import Login from './pages/login';
 import Logout from './pages/logout';
 import Dashboard from './pages/dashboard';
+import Layout from './containers/Layout'
 
 const Routes = () => (
     <BrowserRouter>
     <Switch>
         <Route path="/dashboard" component={Dashboard} />
+        <Route path="/home" name="Home" render={props => <Layout {...props}/>} />
         <Route path="/login" component={Login} />
         <Route path="/logout" component={Logout} />
         <Redirect from="/" to="/login" />
