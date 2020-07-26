32f386de4549753b0620f4c42c34920145874f13
Diego Alfonso <diego.alfonso2@gmail.com>
2020-07-26 02:29:30 -0300

    Cambio el nombre de la vista AppServers por AppServerTokens porque ahora va a haber varias vistas relacionadas a App Servers

--- src/containers/views.js
+++ src/containers/views.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Home = React.lazy(() => import('../views/Home'));
 const Users = React.lazy(() => import('../views/Users'));
-const AppServers = React.lazy(() => import('../views/AppServers'));
+const AppServerTokens = React.lazy(() => import('../views/AppServerTokens'));
 
 const views = [
   { path: "/home",        component: Home,        name: "Home",         icon: "home" },
   { path: "/users",       component: Users,       name: "Users",        icon: "user" , exact: true},
   { name: "App Servers", icon: "server", subItems: [
-    { path: "/app-servers",   component: AppServers,    name: "Tokens"},
+    { path: "/app-server-tokens",   component: AppServerTokens,    name: "Tokens"},
   ]},
   
 ];
