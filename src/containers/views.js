27289b8d53709e59b2301dc04a9477b336709246
Diego Alfonso <diego.alfonso2@gmail.com>
2020-07-26 02:42:55 -0300

    Agrego pagina de estado de App Servers con iframe externo a UptimeRobot

--- src/containers/views.js
+++ src/containers/views.js
@@ -3,12 +3,14 @@ import React from 'react';
 const Home = React.lazy(() => import('../views/Home'));
 const Users = React.lazy(() => import('../views/Users'));
 const AppServerTokens = React.lazy(() => import('../views/AppServerTokens'));
+const AppServerStatus = React.lazy(() => import('../views/AppServerStatus'));
 
 const views = [
   { path: "/home",        component: Home,        name: "Home",         icon: "home" },
   { path: "/users",       component: Users,       name: "Users",        icon: "user" , exact: true},
   { name: "App Servers", icon: "server", subItems: [
     { path: "/app-server-tokens",   component: AppServerTokens,    name: "Tokens"},
+    { path: "/app-server-status",   component: AppServerStatus,    name: "Status"},
   ]},
   
 ];
