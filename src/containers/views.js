b00e873c153d9e8725e8419ca583f9c7ddce6a22
Diego Alfonso <diego.alfonso2@gmail.com>
2020-07-25 22:17:09 -0300

    Agrego opcion de menu Media Resources

--- src/containers/views.js
+++ src/containers/views.js
@@ -3,12 +3,13 @@ import React from 'react';
 const Home = React.lazy(() => import('../views/Home'));
 const Users = React.lazy(() => import('../views/Users'));
 const AppServers = React.lazy(() => import('../views/AppServers'));
+const MediaResources = React.lazy(() => import('../views/MediaResources'));
 
 const views = [
-  { path: "/home",        component: Home,        name: "Home",         icon: "home" },
-  { path: "/users",       component: Users,       name: "Users",        icon: "user" , exact: true},
-  { path: "/app-servers", component: AppServers,  name: "App Servers",  icon: "server" },
-  
+  { path: "/home",            component: Home,            name: "Home",             icon: "home" },
+  { path: "/users",           component: Users,           name: "Users",            icon: "user" , exact: true},
+  { path: "/app-servers",     component: AppServers,      name: "App Servers",      icon: "server" },
+  { path: "/media-resources", component: MediaResources,  name: "Media Resources",  icon: "video-camera"},
 ];
 
 export default views;
